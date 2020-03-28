19044b64af3ea0f189be5eead2583a0384a08af6
darkdraiden14 <jhasrv2830@gmail.com>
2020-03-28 19:01:09 +0530

    Currently in Development phase.

---
+++ app/src/main/java/classActivities/class10/statisticsActivity.java
@@ -0,0 +1,16 @@
+package classActivities.class10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mathflick.R;
+
+public class statisticsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statistics);
+    }
+}
--- app/src/main/java/classActivities/class10Activity.java
+++ app/src/main/java/classActivities/class10Activity.java
@@ -11,9 +11,16 @@ import android.widget.Button;
 import com.example.mathflick.R;
 
 import classActivities.class10.arithProgActivity;
+import classActivities.class10.circleActivity;
+import classActivities.class10.coordinateGeoActivity;
 import classActivities.class10.linearEqnActivity;
 import classActivities.class10.polynomialsActivity;
+import classActivities.class10.probabilityActivity;
+import classActivities.class10.quadEqnActivity;
 import classActivities.class10.realNumbersActivity;
+import classActivities.class10.statisticsActivity;
+import classActivities.class10.surfaceAVActivity;
+import classActivities.class10.triangleActivity;
 import classActivities.class10.trignoActivity;
 
 public class class10Activity extends AppCompatActivity implements View.OnClickListener {
@@ -23,7 +30,8 @@ public class class10Activity extends AppCompatActivity implements View.OnClickLi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_class10);
 
-        Button btnReal, btnPoly, btnLinearEqn, btnTrigno, btnArithProg;
+        Button btnReal, btnPoly, btnLinearEqn, btnQuad, btnArithProg, btnTriangle, btnCoGeo,
+                btnTrigno, btnCircle, btnSurfaceAV, btnStats, btnProb;
 
         overridePendingTransition(R.anim.fadein, R.anim.fadeout);
 
@@ -42,11 +50,33 @@ public class class10Activity extends AppCompatActivity implements View.OnClickLi
         btnLinearEqn = findViewById(R.id.linearEqn);
         btnLinearEqn.setOnClickListener(this);
 
-        btnTrigno = findViewById(R.id.trignometry);
-        btnTrigno.setOnClickListener(this);
+        btnQuad = findViewById(R.id.quadEqn);
+        btnQuad.setOnClickListener(this);
 
         btnArithProg = findViewById(R.id.arithProg);
         btnArithProg.setOnClickListener(this);
+
+        btnTriangle = findViewById(R.id.triangle);
+        btnTriangle.setOnClickListener(this);
+
+        btnCoGeo = findViewById(R.id.coordinateGeometry);
+        btnCoGeo.setOnClickListener(this);
+
+        btnTrigno = findViewById(R.id.trignometry);
+        btnTrigno.setOnClickListener(this);
+
+        btnCircle = findViewById(R.id.circle);
+        btnCircle.setOnClickListener(this);
+
+        btnSurfaceAV = findViewById(R.id.surfaceArea);
+        btnSurfaceAV.setOnClickListener(this);
+
+        btnStats = findViewById(R.id.statistics);
+        btnStats.setOnClickListener(this);
+
+        btnProb = findViewById(R.id.probability);
+        btnProb.setOnClickListener(this);
+
     }
 
 
@@ -68,14 +98,49 @@ public class class10Activity extends AppCompatActivity implements View.OnClickLi
                 startActivity(linearIntent);
                 break;
 
-            case R.id.trignometry:
-                Intent trignoIntent = new Intent(class10Activity.this, trignoActivity.class);
-                startActivity(trignoIntent);
+            case R.id.quadEqn:
+                Intent quadIntent = new Intent(class10Activity.this, quadEqnActivity.class);
+                startActivity(quadIntent);
                 break;
 
             case R.id.arithProg:
                 Intent arithProgIntent = new Intent(class10Activity.this, arithProgActivity.class);
                 startActivity(arithProgIntent);
+                break;
+
+            case R.id.triangle:
+                Intent triangleIntent = new Intent(class10Activity.this, triangleActivity.class);
+                startActivity(triangleIntent);
+                break;
+
+            case R.id.coordinateGeometry:
+                Intent coGeoIntent = new Intent(class10Activity.this, coordinateGeoActivity.class);
+                startActivity(coGeoIntent);
+                break;
+
+            case R.id.trignometry:
+                Intent trignoIntent = new Intent(class10Activity.this, trignoActivity.class);
+                startActivity(trignoIntent);
+                break;
+
+            case R.id.circle:
+                Intent circleIntent = new Intent(class10Activity.this, circleActivity.class);
+                startActivity(circleIntent);
+                break;
+
+            case R.id.surfaceArea:
+                Intent surfaceAVIntent = new Intent(class10Activity.this, surfaceAVActivity.class);
+                startActivity(surfaceAVIntent);
+                break;
+
+            case R.id.statistics:
+                Intent statsIntent = new Intent(class10Activity.this, statisticsActivity.class);
+                startActivity(statsIntent);
+                break;
+
+            case R.id.probability:
+                Intent probIntent = new Intent(class10Activity.this, probabilityActivity.class);
+                startActivity(probIntent);
                 break;
         }
     }
