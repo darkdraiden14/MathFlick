e673112e966e9f9468e35d839de0a7505a67daa1
darkdraiden14 <jhasrv2830@gmail.com>
2020-03-28 18:11:55 +0530

    Currently in Development phase.

--- app/src/main/java/classActivities/class10Activity.java
+++ app/src/main/java/classActivities/class10Activity.java
@@ -11,6 +11,7 @@ import android.widget.Button;
 import com.example.mathflick.R;
 
 import classActivities.class10.arithProgActivity;
+import classActivities.class10.linearEqnActivity;
 import classActivities.class10.polynomialsActivity;
 import classActivities.class10.realNumbersActivity;
 import classActivities.class10.trignoActivity;
@@ -22,9 +23,9 @@ public class class10Activity extends AppCompatActivity implements View.OnClickLi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_class10);
 
-        Button btnReal,btnPoly , btnTrigno, btnArithProg;
+        Button btnReal, btnPoly, btnLinearEqn, btnTrigno, btnArithProg;
 
-        overridePendingTransition(R.anim.fadein,R.anim.fadeout);
+        overridePendingTransition(R.anim.fadein, R.anim.fadeout);
 
         ActionBar class10Bar = getSupportActionBar();
         assert class10Bar != null;
@@ -35,8 +36,11 @@ public class class10Activity extends AppCompatActivity implements View.OnClickLi
         btnReal = findViewById(R.id.realNumber);
         btnReal.setOnClickListener(this);
 
-        btnPoly =findViewById(R.id.polynomial);
+        btnPoly = findViewById(R.id.polynomial);
         btnPoly.setOnClickListener(this);
+
+        btnLinearEqn = findViewById(R.id.linearEqn);
+        btnLinearEqn.setOnClickListener(this);
 
         btnTrigno = findViewById(R.id.trignometry);
         btnTrigno.setOnClickListener(this);
@@ -48,7 +52,7 @@ public class class10Activity extends AppCompatActivity implements View.OnClickLi
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.realNumber:
                 Intent realIntent = new Intent(class10Activity.this, realNumbersActivity.class);
                 startActivity(realIntent);
@@ -57,6 +61,11 @@ public class class10Activity extends AppCompatActivity implements View.OnClickLi
             case R.id.polynomial:
                 Intent polyIntent = new Intent(class10Activity.this, polynomialsActivity.class);
                 startActivity(polyIntent);
+                break;
+
+            case R.id.linearEqn:
+                Intent linearIntent = new Intent(class10Activity.this, linearEqnActivity.class);
+                startActivity(linearIntent);
                 break;
 
             case R.id.trignometry:
