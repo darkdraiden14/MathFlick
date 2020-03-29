2cde62942a269a8b8ef729cce1c9aaad21dc16ad
darkdraiden14 <jhasrv2830@gmail.com>
2020-03-29 19:10:15 +0530

    Currently in Development phase.

--- app/src/main/java/classActivities/class10/circleActivity.java
+++ app/src/main/java/classActivities/class10/circleActivity.java
@@ -2,15 +2,32 @@ package classActivities.class10;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.mathflick.R;
 
-public class circleActivity extends AppCompatActivity {
+import classActivities.class10.circle.areaCircleActivity;
+
+public class circleActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_circle);
+
+        Button btnAreaCircle;
+        btnAreaCircle = findViewById(R.id.areaCircle);
+        btnAreaCircle.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.areaCircle){
+            Intent areaIntent = new Intent(circleActivity.this, areaCircleActivity.class);
+            startActivity(areaIntent);
+        }
     }
 }
