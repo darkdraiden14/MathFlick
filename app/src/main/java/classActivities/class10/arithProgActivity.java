b9e6169c8cadb6325cfa03e81831bbf11b68ea06
darkdraiden14 <jhasrv2830@gmail.com>
2020-04-04 23:44:59 +0530

    Quadratic equation tab works

--- app/src/main/java/classActivities/class10/arithProgActivity.java
+++ app/src/main/java/classActivities/class10/arithProgActivity.java
@@ -4,10 +4,18 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.mathflick.R;
 
-public class arithProgActivity extends AppCompatActivity {
+public class arithProgActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText edtA, edtD, edtN, edtAn, edtSn;
+    private TextView txtResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,45 @@ public class arithProgActivity extends AppCompatActivity {
         arithBar.setDisplayShowHomeEnabled(true);
         arithBar.show();
 
+        Button btnCalc, btnReset;
+
+        btnCalc = findViewById(R.id.APCalc);
+        btnCalc.setOnClickListener(this);
+
+        btnReset = findViewById(R.id.APReset);
+        btnReset.setOnClickListener(this);
+
+        edtA = findViewById(R.id.first_term);
+        edtD = findViewById(R.id.second_term);
+        edtN = findViewById(R.id.numberOfterm);
+        edtAn = findViewById(R.id.nthTerm);
+        edtSn = findViewById(R.id.sumOfTerms);
+        txtResult = findViewById(R.id.APResult);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.APCalc:
+                if (edtA.getText().toString().trim().length() <= 0 || edtD.getText().toString().trim().length() <= 0 ||
+                        edtN.getText().toString().trim().length() <= 0 || edtSn.getText().toString().trim().length() <= 0 ||
+                        edtAn.getText().toString().trim().length() <= 0) {
+                    Toast.makeText(this,"Enter the Values", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(this,"Worked", Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+            case R.id.APReset:
+                edtA.setText("");
+                edtD.setText("");
+                edtAn.setText("");
+                edtSn.setText("");
+                edtN.setText("");
+                txtResult.setText("");
+                break;
+        }
     }
 }
